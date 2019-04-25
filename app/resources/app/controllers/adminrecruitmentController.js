91eb4d24ea89e9346db6f21d2d52b282c81b4ee8
Joe Passanante <j.e.passanante@gmail.com>
2019-04-25 19:18:12 -0400

    deleteAll

--- app/resources/app/controllers/adminrecruitmentController.js
+++ app/resources/app/controllers/adminrecruitmentController.js
@@ -3,13 +3,30 @@ fijiApp.controller('adminrecruitmentController', function($scope, $http) {
     $http.get('/application')
       .then(function(result) {
     $scope.recruitment = result.data;
+    console.log($scope.recruitment);
     });
-    $scope.remove = function(id) {
+    $scope.remove = function(id,reload=true) {
       console.log(id);
       $http.delete('/application?id=' + id)
       .then(function(res) {
-        window.location.reload();
+        if(reload){
+          window.location.reload();
+        }
       });
+    };
+    /**
+     * @typedef {Object} recruitment
+     * @property {String} _id
+     * @property {String} firstName
+     * @property {String} lastName
+     * @property {String} email
+     * @property {String} phone
+     * @param {Array<recruitment>} data 
+     */
+    $scope.deleteAll = function(data){
+      data.forEach(object=>{
+        $scope.remove(object._id,false);
+      })
     };
     $scope.save = function(data, id) {
       //$scope.user not updated yet
